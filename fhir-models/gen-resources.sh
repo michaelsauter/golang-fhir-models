a6cf5b274f8c44e6fd5fbeef498151a06276273d
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2022-08-11 10:19:54 +0200

    Fix gen-resources.sh scripts
    * Fail on non-zero exit code
    * Set version to R4 to avoid error when R4B is downloaded
    * Run `go install` in `fhir-models-gen` to ensure latest generator is
      used

--- fhir-models/gen-resources.sh
+++ fhir-models/gen-resources.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+set -eu
 
-wget -O definitions.zip https://www.hl7.org/fhir/definitions.json.zip
+# Fix version to R4 for now.
+# As R4B has been released in May 2022, we should update to this or at least
+# allow the option to select the version.
+wget -O definitions.zip https://www.hl7.org/fhir/R4/definitions.json.zip
 unzip definitions.zip profiles-resources.json profiles-types.json valuesets.json -d fhir
 rm definitions.zip
 
